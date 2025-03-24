0c6696e583a8d59835637c8f99ff7091b77ae1f2
vipul.kumar <vipul.kumar@igniteconnex.com>
2025-03-24 12:44:19 +0530

    fixed axios

--- src/index.js
+++ src/index.js
@@ -16,7 +16,17 @@ async function crawlWebsite(url) {
   console.log(`Crawling ${url}`);
   try {
     const agent = new https.Agent({ rejectUnauthorized: false });
-    const { data } = await axios.get(url, { httpsAgent: agent });
+
+    const { data } = await axios.get(url, {
+      httpsAgent: agent,
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        Accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5",
+      },
+    });
 
     const $ = cheerio.load(data);
 
